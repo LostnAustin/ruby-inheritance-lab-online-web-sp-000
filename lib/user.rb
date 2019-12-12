64d13087d7410c872c1edbcf5ef5d718d3209ffe
Chad Lengefeld <chadlengefeld@gmail.com>
2019-12-12 16:08:03 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,10 @@
 class User
   attr_accessor :first_name, :last_name
   @@all = []
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
-    @@all << self
-  end
+#  def initialize(first_name, last_name)
+#    @first_name = first_name
+#    @last_name = last_name
+#    @@all << self
+#  end
 
 end
